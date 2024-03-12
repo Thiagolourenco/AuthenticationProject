d54f84343c4b2bbe2767fd8c2bbb0a04074759a8
ThiagoLourenco <thiagolourencosaraiva123@gmail.com>
2024-03-12 19:10:32 -0300

    Initial Commit

---
+++ AuthenticationProject/AuthenticationProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationProjectApp.swift
+//  AuthenticationProject
+//
+//  Created by Thiago Lourenço on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
